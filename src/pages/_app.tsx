8eab572a914fafbe888d7a74a7607336ea6d9b10
jayantna <jonty.nagle@gmail.com>
2025-02-15 10:32:21 +0530

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css';
+import '../app/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { RainbowKitProvider, lightTheme, darkTheme } from '@rainbow-me/rainbowkit';
 
 import { config } from '../wagmi';
 
